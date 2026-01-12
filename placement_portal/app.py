23653112d6a6bbd40ff94fd268e70540932d620e
23f2004545 <23f2004545@ds.study.iitm.ac.in>
2026-01-13 00:08:02 +0530

    Initialize database

--- placement_portal/app.py
+++ placement_portal/app.py
@@ -1,7 +1,15 @@
 from flask import Flask , render_template
-
+from models.db import db
+from config import config
 
 app = Flask(__name__)
+# app.config.from_object(config)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///placement_portal.db'
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
 
 @app.route('/')
 def hello():
---
+++ placement_portal/config.py
@@ -0,0 +1,4 @@
+class config:
+    SECRET_KEY = "secret_key"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///placement_portal.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
